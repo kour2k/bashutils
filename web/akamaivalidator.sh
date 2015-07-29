c8230e26862845e959da9a425703e79c35fea90e
MP <mpedreira@users.noreply.github.com>
2015-07-29 16:13:15 +0200

    Includes new records (like IP address)
    Minor changes in CSV

--- web/akamaivalidator.sh
+++ web/akamaivalidator.sh
@@ -14,7 +14,7 @@ if [ ! -e $listado ]; then
     exit 2
 fi
 
-while read -r line; do  
+while read -r line; do
 	domain=$line
     nslookup $domain|grep edgekey.net > /dev/null
     if [ $? ]; then
@@ -27,14 +27,14 @@ while read -r line; do
             akamai="NON_AKAMAI"
         fi
     fi
-    curl -A "Mozilla/5.0" -s -I $domain |awk -v akamai=$akamai -v domain=$domain ' BEGIN{ RS="" }
-            /200 OK/{ print akamai";"domain";"$2}
-            /301 Moved Permanently/ { print akamai";"domain";"$2";"$10}
-            /302 Moved Temporarily/ { print akamai";"domain";"$2";"$10}
-            /400 Bad Request/ { print akamai";"domain";"$2";Not included on AKAMAIs configuration"}
-            /503 Service Unavailable/ { print akamai";"domain";"$2"; WAF Configuration?" }
+		ip=`nslookup $domain|grep Address|grep -v "#53"|awk '{print $2}'`
+    curl -A "Mozilla/5.0" -s -I $domain |awk -v akamai=$akamai -v domain=$domain -v ip=$ip ' BEGIN{ RS="" }
+            /200 OK/{ print akamai";"domain";"ip";"$2}
+            /301 Moved Permanently/ { print akamai";"domain";"ip";"$2";"$8}
+            /302 Moved Temporarily/ { print akamai";"domain";"ip";"$2";"$8}
+            /400 Bad Request/ { print akamai";"domain";"ip";"$2";Not included on AKAMAIs configuration"}
+            /503 Service Unavailable/ { print akamai";"domain";"ip";"$2"; WAF Configuration?" }
             ' >> $tmp
 done < $listado
 #close $inFile
 mv $tmp $result
-
